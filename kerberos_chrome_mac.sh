f4aea8409e9564b00332ae8e8bf39581eef14490
Emil Kleszcz <emil.kleszcz@cern.ch>
2018-10-17 14:49:08 +0200

    Add prefix for all webpages for a chosen domain for Kerberos-Chrome script

--- kerberos_chrome_mac.sh
+++ kerberos_chrome_mac.sh
@@ -3,8 +3,8 @@ adfs_alias=$1
 echo "${adfs_alias}"
 loggedInUser=`python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");'`
 echo $loggedInUser
-sudo -u $loggedInUser defaults write com.google.Chrome AuthServerWhitelist "${adfs_alias}"
-sudo -u $loggedInUser defaults write com.google.Chrome AuthNegotiateDelegateWhitelist "${adfs_alias}"
+sudo -u $loggedInUser defaults write com.google.Chrome AuthServerWhitelist "*.${adfs_alias}"
+sudo -u $loggedInUser defaults write com.google.Chrome AuthNegotiateDelegateWhitelist "*.${adfs_alias}"
 echo $(date)
 exit 0
 
