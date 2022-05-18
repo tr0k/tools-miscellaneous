4cee930224d9602e33d6ddd1b0b498a29f2503b1
Emil Kle <6087242+tr0k@users.noreply.github.com>
2022-05-18 16:47:08 +0200

    Update kerberos_chrome_mac.sh - add an example run

--- kerberos_chrome_mac.sh
+++ kerberos_chrome_mac.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+
+# Run example: ./kerberos_chrome_mac.sh cern.ch
+# To verify chrome policies: chrome://policy/
+
 adfs_alias=$1
 echo "${adfs_alias}"
 loggedInUser=`python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");'`
@@ -7,5 +11,3 @@ sudo -u $loggedInUser defaults write com.google.Chrome AuthServerWhitelist "*.${
 sudo -u $loggedInUser defaults write com.google.Chrome AuthNegotiateDelegateWhitelist "*.${adfs_alias}"
 echo $(date)
 exit 0
-
-# To verify chrome policies: chrome://policy/
