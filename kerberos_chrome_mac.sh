1725c57b9b91f78b16e1c503658a53e1f812582f
Emil Kle <6087242+tr0k@users.noreply.github.com>
2022-05-18 17:06:55 +0200

    Update kerberos_chrome_mac.sh script
    Adapting to the latest changes of the parameters in Chrome 101

--- kerberos_chrome_mac.sh
+++ kerberos_chrome_mac.sh
@@ -2,12 +2,20 @@
 
 # Run example: ./kerberos_chrome_mac.sh cern.ch
 # To verify chrome policies: chrome://policy/
+# Restart of the webbrowser is needed too
 
-adfs_alias=$1
+adfs_alias="${1:-cern.ch}"
 echo "${adfs_alias}"
 loggedInUser=`python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");'`
 echo $loggedInUser
-sudo -u $loggedInUser defaults write com.google.Chrome AuthServerWhitelist "*.${adfs_alias}"
-sudo -u $loggedInUser defaults write com.google.Chrome AuthNegotiateDelegateWhitelist "*.${adfs_alias}"
+
+# Deprecated since Chrome 101 (at least)
+#sudo -u $loggedInUser defaults write com.google.Chrome AuthServerWhitelist "*.${adfs_alias}"
+#sudo -u $loggedInUser defaults write com.google.Chrome AuthNegotiateDelegateWhitelist "*.${adfs_alias}"
+
+# New options to set up
+sudo -u $loggedInUser defaults write com.google.Chrome AuthServerAllowlist "*.${adfs_alias}"
+sudo -u $loggedInUser defaults write com.google.Chrome AuthNegotiateDelegateAllowlist "*.${adfs_alias}"
+
 echo $(date)
 exit 0
